e0343336e2468e77f3c35b5fb6198cfb202d04bb
Creapp <tech@creapp.us>
2016-03-07 22:35:54 +0100

    begin app project

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('creapp3App', [
+  'creapp3App.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/creapp3-dev'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
