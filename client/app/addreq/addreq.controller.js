288ee23d6fee7f08b459e245c078a470e6899e65
Creapp <tech@creapp.us>
2016-04-02 16:48:18 +0200

    cancel and delete work on add/edit req form

--- client/app/addreq/addreq.controller.js
+++ client/app/addreq/addreq.controller.js
@@ -28,6 +28,17 @@ angular.module('creapp3App')
       // active: true
     };
 
+    //funciton to show toasts
+    var showToast = function(msg){
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent(msg)
+        .position('top left')
+        .parent(document.getElementById('toasts'))
+        .hideDelay(3000)
+      );
+    };
+
     // put empty req or req to edit in scope into scope
     if ($stateParams.edit.editMode){
       $scope.addmode = false;
@@ -56,61 +67,45 @@ angular.module('creapp3App')
     // post the req
     $scope.addReq = function(){
       // other properties such as user, date creation are added server side
-      // set up parent element where toast will drop from
-      var parentEl = document.getElementById('toasts');
       // post req to server then drop toast
       $http.post('/api/buyreqs', $scope.req).then(function(){
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Your requirement was saved !')
-            .position('top right')
-            .parent(parentEl)
-            .hideDelay(5000)
-        );
+        showToast('Your requirement was saved !');
         $scope.req = _.cloneDeep(emptyReq);
         $state.go('myreqs');
       }, function(){
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('There was an issue in saving your requisition')
-            .position('top left')
-            .position(parentEl)
-            .hideDelay(5000)
-        );
+        showToast('There was an issue in saving your requisition');
       });
     };
 
     // save req changes
     $scope.saveEdit = function(){
-      // set up parent element where toast will drop from
-      var parentEl = document.getElementById('toasts');
       // put req to server then drop toast
       $http.put('/api/buyreqs/' + $scope.req._id, $scope.req).then(function(){
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Your requirement edits were saved !')
-            .position('top left')
-            .parent(parentEl)
-            .hideDelay(5000)
-        );
+        showToast('Your requirement edits were saved !');
         $scope.req = _.cloneDeep(emptyReq);
         $state.go('myreqs');
       }, function(){
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('There was an issue in saving your edits')
-            .position('top right')
-            .position(parentEl)
-            .hideDelay(5000)
-        );
+        showToast('There was an issue in saving your edits');
       });
     };
 
+    $scope.deleteReq = function(){
+      $http.delete('/api/buyreqs/' + $scope.req._id).then(function(){
+        showToast('Your requirement was deleted !');
+        $scope.req = _.cloneDeep(emptyReq);
+        $state.go('myreqs');
+      }, function(){
+        showToast('There was an issue deleting your requisition');
+      });
+    };
 
     // clear all for button click
-    $scope.cleanReq = function(){
+    $scope.cancel = function(){
       $scope.req = _.cloneDeep(emptyReq);
+      showToast('No changes were made');
+      $state.go('myreqs');
     };
+
 
     // clear polygons for button click
     $scope.clearPolygons = function(){
