e77242e0b02d5f497949d6213f2f8ac3d77de075
Creapp <tech@creapp.us>
2016-06-05 16:15:58 +0200

    removed link function of list directive

--- client/app/buyreqs/reqList/reqList.directive.js
+++ client/app/buyreqs/reqList/reqList.directive.js
@@ -4,15 +4,6 @@ angular.module('creapp3App')
   .directive('reqList', function () {
     return {
       templateUrl: 'app/buyreqs/reqList/reqList.html',
-      restrict: 'E',
-      link: function (scope, element, attrs) {
-
-        scope.showfilter = false;
-        scope.openfilter = function(){
-          console.log('toggle');
-          scope.showfilter = !scope.showfilter;
-        };
-
-      }
+      restrict: 'E'
     };
   });
