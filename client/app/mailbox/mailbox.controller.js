a50065f6f1b2118ab71f29e68c9d6df1697b8922
Creapp <tech@creapp.us>
2016-06-26 19:39:37 +0200

    entirely reformatted mailbox

--- client/app/mailbox/mailbox.controller.js
+++ client/app/mailbox/mailbox.controller.js
@@ -1,52 +1,51 @@
-'use strict';
-(function(){
-
-function MailboxComponent($scope, $http, $mdToast) {
-  $scope.selected = null;
-
-  $scope.toggleMsg = function(msg){
-    if (!$scope.selected){
-      $scope.selected = msg;
-    } else {
-      if ($scope.selected._id === msg._id){
-        $scope.selected = null;
-      }
-    }
-  };
-
-  var showToast = function(msg){
-    $mdToast.show(
-      $mdToast.simple()
-      .textContent(msg)
-      .position('top left')
-      .parent(document.getElementById('toasts'))
-      .hideDelay(3000)
-    );
-  };
-
-  var getMails = function(){
-    $http.get('/api/mails').then(response => {
-      $scope.messages = response.data;
-    });
-  };
-
-  $scope.deleteMsg = function(msg){
-    $http.delete('/api/mails/' + msg._id).then(response => {
-      showToast('the message was deleted');
-      getMails();
-      $scope.selected = null;
-    });
-  };
-
-
-  getMails();
-
-}
-
 angular.module('creapp3App')
-  .component('mailbox', {
-    templateUrl: 'app/mailbox/mailbox.html',
-    controller: MailboxComponent
-  });
+  .controller('mailboxCtrl', function ($scope, $http, $mdToast, $mdSidenav, $mdMedia) {
 
-})();
+    $scope.main = {};
+    $scope.main.selected = null;
+    $scope.main.openedMailList = true;
+
+    $scope.main.selectMsg = function(msg){
+      $scope.main.selected = msg;
+      if (!$scope.main.bigScreen){
+        $mdSidenav('mailList').toggle();
+      }
+    };
+
+    $scope.main.openMailList = function(){
+      $scope.main.openedMailList = !$scope.main.openedMailList;
+    }
+
+    $scope.$watch(function() { return $mdMedia('gt-xs'); }, function(big) {
+      $scope.main.bigScreen = big;
+    });
+
+    var showToast = function(msg){
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent(msg)
+        .position('top left')
+        .parent(document.getElementById('toasts'))
+        .hideDelay(3000)
+      );
+    };
+
+    var getMails = function(){
+      $http.get('/api/mails').then(response => {
+        $scope.main.messages = response.data;
+        $scope.main.selected = $scope.main.messages[0];
+      });
+    };
+
+    $scope.main.deleteMsg = function(){
+      $http.delete('/api/mails/' + $scope.main.selected._id).then(response => {
+        showToast('the message was deleted');
+        getMails();
+        $scope.main.selected = null;
+      });
+    };
+
+
+    getMails();
+
+  });
--- client/app/mailbox/mailbox.js
+++ client/app/mailbox/mailbox.js
@@ -4,7 +4,25 @@ angular.module('creapp3App')
   .config(function ($stateProvider) {
     $stateProvider
       .state('mailbox', {
+        // url: '/mailbox',
+        templateUrl: 'app/mailbox/mailbox.html',
+        controller: 'mailboxCtrl',
+        sp: {
+          authenticate: true
+        }
+      })
+      .state('mailbox.views', {
         url: '/mailbox',
-        template: '<mailbox></mailbox>'
+        views: {
+          'list' : {
+            templateUrl: 'app/mailbox/mailboxList.html',
+          },
+          'detail' : {
+            templateUrl: 'app/mailbox/mailboxDetail.html',
+          }
+        },
+        sp: {
+          authenticate: true
+        }
       });
   });
