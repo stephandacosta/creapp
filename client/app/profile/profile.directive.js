dbd78ca2e95d6e4767d9f4c626611571e5e73006
Creapp <tech@creapp.us>
2016-07-15 01:35:35 +0200

    moved mail button into own directive

--- client/app/profile/profile.directive.js
+++ client/app/profile/profile.directive.js
@@ -4,35 +4,6 @@ angular.module('creapp3App')
   .directive('profile', function ($mdDialog) {
     return {
       templateUrl: 'app/profile/profileView.html',
-      restrict: 'E',
-      scope: {
-        user: '=user',
-        buyreq: '=buyreq'
-      },
-      link: function (scope, element, attrs) {
-        // if (scope.user=='me'){
-        //   // scope.user
-        // } else {
-        //   // scope.user =
-        // }
-
-        scope.showMailForm = function(ev){
-          $mdDialog.show({
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose:true,
-            fullscreen: true,
-            templateUrl:'app/mailForm/mailForm.html',
-            locals: {
-              userId: scope.user.userId,
-              brokerName: scope.user.fullName,
-              buyreqId: scope.buyreq._id,
-              buyreqTitle: scope.buyreq.title
-            },
-            controller: 'mailFormCtrl'
-          });
-        };
-
-      }
+      restrict: 'E'
     };
   });
---
+++ client/components/mailbutton/mailbutton.directive.js
@@ -0,0 +1,34 @@
+angular.module('creapp3App')
+  .directive('mailbutton', function ($mdDialog, buyreqs) {
+    return {
+      templateUrl: 'components/mailbutton/mailbutton.html',
+      restrict: 'E',
+      scope: {
+        broker: '=broker',
+        buyreq: '=buyreq'
+      },
+      link: function (scope, element, attrs) {
+
+        scope.showMailForm = function(ev){
+
+          var selectedReq = buyreqs.getSelectedReq();
+
+          $mdDialog.show({
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: true,
+            templateUrl:'app/mailForm/mailForm.html',
+            locals: {
+              userId: scope.broker.userId || scope.broker.href.substr(scope.broker.href.lastIndexOf('/') + 1),
+              brokerName: scope.broker.fullName,
+              buyreqId: selectedReq._id || '',
+              buyreqTitle: selectedReq.title || 'no specific buy requirement'
+            },
+            controller: 'mailFormCtrl'
+          });
+        };
+
+      }
+    };
+  });
