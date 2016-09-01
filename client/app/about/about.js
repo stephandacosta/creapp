e3c1cbf75c9e753f2b65a3db4fcf01c59f67475a
Creapp <tech@creapp.us>
2016-09-01 03:37:54 +0200

    added about pages

---
+++ client/app/about/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('creapp3App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+      });
+  });
