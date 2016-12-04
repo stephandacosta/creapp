b40186563be8fbff247a362ba9db62a4f2e8ec5b
Creapp <tech@creapp.us>
2016-12-04 18:38:32 +0100

    removed unneeded code in profile view directive and changed link function to controller

--- client/app/profile/profileView.directive.js
+++ client/app/profile/profileView.directive.js
@@ -9,34 +9,31 @@ angular.module('creapp3App')
         broker: '=user',
         buyreq: '=buyreq'
       },
-      link: function(scope,el,attrs){
+      controller: function($scope){
 
         var userId;
 
-        scope.$watch(function(){
-          return scope.broker===undefined;
+        $scope.$watch(function(){
+          return $scope.broker===undefined;
         }, function(isUndef){
           if (!isUndef){
-            if (scope.broker.userId){
-              userId = scope.broker.userId
+            if ($scope.broker.userId){
+              userId = $scope.broker.userId
             } else {
-              userId = scope.broker.href.substr(scope.broker.href.lastIndexOf('/') + 1);
+              userId = $scope.broker.href.substr($scope.broker.href.lastIndexOf('/') + 1);
             }
-            scope.brokerpic = pictureuploadService.getBrokerPictureLink(userId);
+            $scope.brokerpic = pictureuploadService.getBrokerPictureLink(userId);
           }
         });
 
-        scope.getDefaultPic = function(img){
-          console.log('default pic');
+        $scope.getDefaultPic = function(img){
           //avoid infinite loop
           if (arguments.length>0){
             img.onerror=null;
           }
           // set default image
-          scope.brokerpic = '../../assets/images/user-default.png';
-          scope.edit.hasPicStaged = false;
-          scope.edit.hasPicCommitted = false;
-          scope.$apply();
+          $scope.brokerpic = '../../assets/images/user-default.png';
+          $scope.$apply();
         };
 
       }
